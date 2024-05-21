f5c0d09fbf5eedb03dcf4cac00eba767fe4765b1
Mickey78-225 <ehuimichelangearmel@gmail.com>
2024-05-21 07:22:36 +0200

    Version 00.011

--- JS/script.js
+++ JS/script.js
@@ -9,34 +9,34 @@ function handleScroll() {
     }
 }
 
-function addClassOnHover() {
-    const composant_button = document.querySelector("button")
-    const buttonClassName = composant_button.classList.value;
-    if (buttonClassName == ".button__vert") {
-        button.addEventListener("mouseover", () => {
-            // button.classList.add(className);
-            console.log(buttonClassName);
-            console.log('in');
-        });
-        button.addEventListener("mouseout", () => {
-            // button.classList.remove(className);
-            console.log(buttonClassName );
-            console.log('out');
-        });
-    }
+// function addClassOnHover() {
+//     const composant_button = document.querySelector("button")
+//     const buttonClassName = composant_button.classList.value;
+//     if (buttonClassName == ".button__vert") {
+//         button.addEventListener("mouseover", () => {
+//             // button.classList.add(className);
+//             console.log(buttonClassName);
+//             console.log('in');
+//         });
+//         button.addEventListener("mouseout", () => {
+//             // button.classList.remove(className);
+//             console.log(buttonClassName );
+//             console.log('out');
+//         });
+//     }
 
-    // button.addEventListener("mouseover", () => {
-    //     // button.classList.add(className);
-    //     console.log(buttonClassName);
-    //     console.log('in');
-    // });
-    // button.addEventListener("mouseout", () => {
-    //     // button.classList.remove(className);
-    //     console.log(buttonClassName );
-    //     console.log('out');
-    // });
-}
+//     button.addEventListener("mouseover", () => {
+//         // button.classList.add(className);
+//         console.log(buttonClassName);
+//         console.log('in');
+//     });
+//     button.addEventListener("mouseout", () => {
+//         // button.classList.remove(className);
+//         console.log(buttonClassName );
+//         console.log('out');
+//     });
+// }
 
-// Exemple d'utilisation :
-addClassOnHover("monBouton", "hovered");
+// // Exemple d'utilisation :
+// addClassOnHover("monBouton", "hovered");
 document.addEventListener("scroll", handleScroll);
