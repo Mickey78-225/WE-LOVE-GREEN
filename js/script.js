ab8e93013d614c2cba0730bfc196d6381eab28ca
Mickey78-225 <ehuimichelangearmel@gmail.com>
2024-05-24 13:32:36 +0200

    Version 00.00 Ajout de: ...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var splide = new Splide(".splide", {
+        width: "100%",
+        arrows: false,
+        pagination: false,
+    });
+    splide.mount();
+    
+});
+
 function handleScroll() {
     const nav = document.querySelector("nav");
     if (window.scrollY > 0) {
@@ -6,45 +16,5 @@ function handleScroll() {
         nav.classList.remove("scrolled");
     }
 }
-
-// function afficherFestivalDeroulant() {
-//     const festival = document.getElementById(festival);
-//     const menu_festival = document.getElementsByClassName(menu_festival)
-//     if  (window.onclick(festival)) {
-//         menu_festival.style.display = "block";
-//     }
-// }
-
-
-// function addClassOnHover() {
-//     const composant_button = document.querySelector("button")
-//     const buttonClassName = composant_button.classList.value;
-//     if (buttonClassName == ".button__vert") {
-//         button.addEventListener("mouseover", () => {
-//             // button.classList.add(className);
-//             console.log(buttonClassName);
-//             console.log('in');
-//         });
-//         button.addEventListener("mouseout", () => {
-//             // button.classList.remove(className);
-//             console.log(buttonClassName );
-//             console.log('out');
-//         });
-//     }
-
-//     button.addEventListener("mouseover", () => {
-//         // button.classList.add(className);
-//         console.log(buttonClassName);
-//         console.log('in');
-//     });
-//     button.addEventListener("mouseout", () => {
-//         // button.classList.remove(className);
-//         console.log(buttonClassName );
-//         console.log('out');
-//     });
-// }
-
-// // Exemple d'utilisation :
-// addClassOnHover("monBouton", "hovered");
 
 document.addEventListener("scroll", handleScroll);
